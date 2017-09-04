bc6a44f7fc0cb098c9c28714b89c0a003dbfdb65
vaibhavivijayakumar <bvvaibhavi@gmail.com>
2017-09-04 13:03:33 +0530

    First commit

---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+# File: guessinggame.sh
+
+ guessinggame()
+{
+
+n1=$(ls -1|wc -l)
+guesses=1
+echo -n "Guess the number of files in current directory:"
+while read n2;
+do
+if [[$n2 -eq $n1 ]]
+then
+break;
+else
+echo
+if [[ $n2 -gt $n1 ]]
+then
+echo -n "Sorry, your guess is too low. New guess:"
+fi
+fi
+guesses=$((guesses+1))
+
+done
+echo "Good job! It took you $guesses guesses to get the right number."
+}
+
+echo "Script starts.."
+echo "calling guessinggame function"
+guessinggame
